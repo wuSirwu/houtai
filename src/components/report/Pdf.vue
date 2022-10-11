2fe3b25522fc0e17fdf5bf25e69dd4f551503888
吴迟 <2918373602@qq.com>
2022-10-11 17:30:02 +0800

    first commit

---
+++ src/components/report/Pdf.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <breadcrumb-nav>
+      <template v-slot:firstMenu>数据统计</template>
+      <template v-slot:secondMenu>数据报表</template>
+    </breadcrumb-nav>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
